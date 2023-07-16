078a6ef00a13794f1629335cf39d7e71807a9c89
fyre87 <wpattie@comcast.net>
2023-07-16 12:56:29 -0500

    hi

--- Simulation.py
+++ Simulation.py
@@ -1,5 +1,15 @@
 import pybullet as p
+import time
 
+#Create world
 physicsClient = p.connect(p.GUI)
+#Get rid of sidebars
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
+for i in range(0, 1000):
+    print(i)
+    p.stepSimulation()
+    time.sleep(1/60)
+
+#Disconnect world
 p.disconnect()
