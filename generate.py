b46c4d6fffdea23b7365f6c1c94a7db770eb47b0
fyre87 <wpattie@comcast.net>
2023-07-17 23:15:39 -0500

    finished joints

--- generate.py
+++ generate.py
@@ -9,12 +9,17 @@ z = 0.5
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.Send_Cube(name="Box", pos=[x-5,y,z] , size=[length,width,height])
     pyrosim.End()
+    
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[x-3,y,z] , size=[length,width,height])
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5] , size=[length,width,height])
+    pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0.5,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[0.5,0,-0.5] , size=[length,width,height])
+    pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [-0.5,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[-0.5,0,-0.5] , size=[length,width,height])
     pyrosim.End()
 
 Create_World()
